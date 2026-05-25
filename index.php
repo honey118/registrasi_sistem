62c28f669a46b718cf97a05d76a7ed6ad2258726
honey118 <hannych819@gmail.com>
2026-05-25 08:44:51 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+
+include 'koneksi.php';
+
+if (isset($_POST['submit'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $email = $_POST['email'];
+
+    $eksekusi = "INSERT INTO tb_users (nama, password, email) VALUES ('$username', '$password', '$email')";
+    $query = mysqli_query($koneksi, $eksekusi);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Registrasi</title>
+</head>
+<body>
+  <h1>Selamat Datang!</h1>
+  <form method="POST">
+    <div>
+    <label>Username:</label>
+    <input type="text" id="username" name="username" required>
+    </div>
+    
+    <div>
+    <label>Password:</label>
+    <input type="password" id="password" name="password" required>
+    </div>
+
+    <div>
+    <label>Email:</label>
+    <input type="email" id="email" name="email" required>
+    </div>
+
+    <div>
+    <input type="submit" value="Login" name="submit">
+    </div>
+  </form>
+</body>
+</html>
---
+++ koneksi.php
@@ -0,0 +1,14 @@
+<?php
+
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "db_registrasi_sistem";
+
+$koneksi = mysqli_connect($host, $username, $password, $database);
+
+if (!$koneksi) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+
+?>
