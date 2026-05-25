6b727a73ff7e1e93ef7f0a98f798c8a7ce32cf92
honey118 <hannych819@gmail.com>
2026-05-25 08:51:42 +0700

    commit kedua

--- index.php
+++ index.php
@@ -18,6 +18,7 @@ if (isset($_POST['submit'])) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Registrasi</title>
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
   <h1>Selamat Datang!</h1>
